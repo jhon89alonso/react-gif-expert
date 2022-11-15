3d32d203043e069150ecaf3ca8952df55fb7aa25
unknown <jalonso@edec.com.co>
2022-11-14 19:44:09 -0500

    se agrega hook use state de prueba

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,9 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 
 export const GifExpertApp = () => {
+  const [categories, setCategories] = useState(["One Punch", 'Dragon Ball']);
+
+  console.log(categories);
   return (
     <>
       <h1>GifExpertApp</h1>
+
+      <ol>
+        {categories.map(category => {
+            return <li key={category}>{category}</li>
+        })}
+  
+      </ol>
     </>
   );
 };
