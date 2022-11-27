3c0f8c920870e2c091586923d1f9aaf6bb592a18
unknown <jalonso@edec.com.co>
2022-11-27 13:29:46 -0500

    se crea nuevo componente y se hace llamado al mismo en componente giftexpertapp

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,18 +1,30 @@
 import React, { useState } from "react";
+import { AddCategory } from "./components/AddCategory";
+
 
 export const GifExpertApp = () => {
-  const [categories, setCategories] = useState(["One Punch", 'Dragon Ball']);
+  const [categories, setCategories] = useState(["One Punch", "Dragon Ball"]);
+
+  const onAddCategory = () => {
+    console.log("gate");
+    // categories.push("gate");
+    setCategories([...categories, "Gate"]);
+    //setCategories(cat =>[...categories, "Gate"]);
+  };
 
   console.log(categories);
   return (
     <>
       <h1>GifExpertApp</h1>
+      <AddCategory />
+      
+
+      <button onClick={onAddCategory}>Agregar</button>
 
       <ol>
-        {categories.map(category => {
-            return <li key={category}>{category}</li>
+        {categories.map((category) => {
+          return <li key={category}>{category}</li>;
         })}
-  
       </ol>
     </>
   );
