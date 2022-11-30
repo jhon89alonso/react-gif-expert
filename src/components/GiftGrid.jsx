39b57d09b4dc3f82a68221f336a1434f547128ee
unknown <jalonso@edec.com.co>
2022-11-29 19:47:41 -0500

    se realiza modificacion de componentes y se crea nuevo componente getgifts

--- src/components/GiftGrid.jsx
+++ src/components/GiftGrid.jsx
@@ -1,11 +1,12 @@
+import { getGifts } from "../helpers/getGifts";
 
 
-export const GiftGrid = ({category}) => {
+export const GiftGrid = ({ category }) => {
+  getGifts(category);
   return (
     <>
-    <h3>{category}</h3>
-    <p>hola mundo</p>
-    
+      <h3>{category}</h3>
+      <p>hola mundo</p>
     </>
-  )
-}
+  );
+};
---
+++ src/helpers/getGifts.js
@@ -0,0 +1,17 @@
+export const getGifts = async (category) => {
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=Zc2RYTHPkhYsHNxD4qky7jMPhC8JXLVl&q=${category}&limit=20`;
+  
+    const resp = await fetch(url);
+  
+    const { data } = await resp.json();
+  
+    const gifts = data.map((img) => ({
+      id: img.id,
+      title: img.title,
+      url: img.images.downsized_medium.url,
+    }));
+    console.log(gifts);
+  
+    return gifts;
+  };
+  
