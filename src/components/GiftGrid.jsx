7baafe21a07c3e3caf3162f82003e242e0e5ddf9
unknown <jalonso@edec.com.co>
2022-12-01 20:43:40 -0500

    se modifca componente gift grid para validacion de isloading y mostrarlo en vista

--- src/components/GiftGrid.jsx
+++ src/components/GiftGrid.jsx
@@ -8,6 +8,7 @@ export const GiftGrid = ({ category }) => {
   return (
     <>
       <h3>{category}</h3>
+      {isLoading && <h2>Cargando ...</h2>}
       {/* <h5>{counter} </h5>
       <button onClick={() => setCounter(counter + 1)}>+1</button> */}
       <div className="card-grid">
