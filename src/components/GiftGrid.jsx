a6d322203b94714ad08f1b1d7b1528cc125e8dca
unknown <jalonso@edec.com.co>
2022-12-01 20:36:08 -0500

    se crea custom hook  y se modifica giftgrid

--- src/components/GiftGrid.jsx
+++ src/components/GiftGrid.jsx
@@ -1,32 +1,17 @@
 import { useState, useEffect } from "react";
 import { getGifts } from "../helpers/getGifts";
+import { UseFetchGifts } from "../hooks/UseFetchGifts";
 import { GifItem } from "./GifItem";
 
 export const GiftGrid = ({ category }) => {
-  // const [counter, setCounter] = useState(10);
-  const [images, setImages] = useState();
-
-  const getImages = async () => {
-    const newImages = await getGifts(category);
-    setImages(newImages);
-  };
-
-  useEffect(() => {
-    getImages();
-  }, []);
-
-  //getGifts(category);
-
+  const { images, isLoading } = UseFetchGifts(category);
   return (
     <>
       <h3>{category}</h3>
       {/* <h5>{counter} </h5>
       <button onClick={() => setCounter(counter + 1)}>+1</button> */}
       <div className="card-grid">
-        {images &&
-          images.map((image) => 
-          <GifItem key={image.id} {...image}/>
-          )}
+        {images && images.map((image) => <GifItem key={image.id} {...image} />)}
       </div>
     </>
   );
